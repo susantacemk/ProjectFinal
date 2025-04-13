8489cfc9a29f3c0eb65765b1dfc4a9dd0dafbbf9
susantacemk <cse21083@cemk.ac.in>
2025-04-13 12:38:52 +0530

    Initial Changes

---
+++ mainwindow.cpp
@@ -0,0 +1,27 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QLabel>
+#include <QDateTime>
+#include <QTimer>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    // Assuming `this` is a QMainWindow
+    QLabel *dateTimeLabel = new QLabel(this);  // Create a QLabel to display date and time
+    ui->statusBar->addPermanentWidget(dateTimeLabel);  // Add it to the status bar
+
+    // Create a timer to update the date and time every second
+    QTimer *timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, [=]() {
+        QString dateTimeString = QDateTime::currentDateTime().toString("yyyy-MM-dd HH:mm:ss");
+        dateTimeLabel->setText(dateTimeString);
+    });
+    timer->start(1000);  // Update every 1 second (1000 ms)
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
